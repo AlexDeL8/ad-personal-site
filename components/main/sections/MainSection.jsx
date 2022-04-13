aea8d7e8d4219dc38afaad4187752381b0ad5e83
Alex De Luca <alexnaj88@gmail.com>
2022-04-13 13:00:04 -0400

    main image tag now presenting as background but lost parallax

--- components/main/sections/MainSection.jsx
+++ components/main/sections/MainSection.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import Image from "next/image";
 import mainStyles from "../../../styles/Home.module.sass";
+import MainBackgroundImage from "/public/main/wood-desk-bw.jpg";
 import ProfilePicture from "/public/main/ad-profile.jpg";
 
 import ConnectLinks from "../../connect/ConnectLinks.jsx";
@@ -7,6 +9,16 @@ import ConnectLinks from "../../connect/ConnectLinks.jsx";
 const MainSection = () => {
     return (
         <section className={mainStyles.section} id={mainStyles.mainSection}>
+            <div className={mainStyles.mainBackgroundImgDiv}>
+                <Image
+                    className={mainStyles.mainBackgroundImg}
+                    src={MainBackgroundImage.src}
+                    layout="fill"
+                    objectFit="cover"
+                    objectPosition="center"
+                    quality={100}
+                />
+            </div>
             <img
                 className={mainStyles.profilePic}
                 alt="Alex De Luca profile picture"
