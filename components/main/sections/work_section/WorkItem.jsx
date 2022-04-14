27863c42de4ec06b6ee0604b47ac3ced38ea46ec
Alex De Luca <alexnaj88@gmail.com>
2022-04-13 22:42:49 -0400

    reworking the link to be the whole workItem and removing workItem link icon

--- components/main/sections/work_section/WorkItem.jsx
+++ components/main/sections/work_section/WorkItem.jsx
@@ -1,27 +1,20 @@
 import React from "react";
 import mainStyles from "../../../../styles/Home.module.sass";
-import GithubIcon from "/public/connect/github-icon-white.png";
-import MediumIcon from "/public/connect/medium-icon-white.png";
 
 const WorkItem = (props) => {
-    const itemLinkIcon =
-        props.itemType === "Projects" ? GithubIcon.src : MediumIcon.src;
-
     return (
         <li className={mainStyles.workItem}>
-            <img className={mainStyles.workItemImg} src={props.itemImg} />
-            <div className={mainStyles.workItemInfo}>
-                <p className={mainStyles.workItemTitle}>{props.itemTitle}</p>
-                <p className={mainStyles.workItemDescription}>
-                    {props.itemDesc}
-                </p>
-                <a href={props.itemLink} target="_blank">
-                    <img
-                        className={mainStyles.workItemIcon}
-                        src={itemLinkIcon}
-                    />
-                </a>
-            </div>
+            <a href={props.itemLink} target="_blank">
+                <img className={mainStyles.workItemImg} src={props.itemImg} />
+                <div className={mainStyles.workItemInfo}>
+                    <p className={mainStyles.workItemTitle}>
+                        {props.itemTitle}
+                    </p>
+                    <p className={mainStyles.workItemDescription}>
+                        {props.itemDesc}
+                    </p>
+                </div>
+            </a>
         </li>
     );
 };
